5cac625a5916b11c258a328063831593d4ed130f
mvadgama <manish@chillibean.tv>
2023-03-30 16:58:48 +0100

    added push to git

--- container-automate.sh
+++ container-automate.sh
@@ -1,32 +1,17 @@
 #!/bin/bash
-
-# #echo "installing essentails test..."
-# apt-get -y update && \
-# apt-get -y install git && \
-# which git
-
 echo "create namespace folder name..."
 namespace_id=$RANDOM
 echo "namespace ID: $namespace_id"
 
-pwd
-ls -l
+values_file="my-mongodb/environments/development/values-${namespace_id}.yaml"
 
-
-
-
-
-# cd /tmp
-# git clone https://github.com/Chillibean/manish-argocd-test.git
-# values_file="/tmp/manish-argocd-test/my-mongodb/environments/development/values-${namespace_id}.yaml"
-
-# if [ -f "$values_file" ]; then
-#     echo "$values_file exists."
-#     exit 1
-# else 
-#     echo "Creating helm values file $values_file"
-#     touch $values_file
-#     git add $values_file
-#     git commit -m "Added values file for namespace ${namespace_id}"
-#     git push
-# fi
+if [ -f "$values_file" ]; then
+    echo "$values_file exists."
+    exit 1
+else 
+    echo "Creating helm values file in $values_file"
+    touch $values_file
+    git add $values_file
+    git commit -m "Added values file for namespace ${namespace_id}"
+    git push
+fi
